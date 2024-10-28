29ccff00958e7215363139123d5686e3ae9955d7
AC25-C <arem_canbaz@icloud.com>
2024-10-28 23:40:45 +0100

    jeg har lavet wishItem objektet inde i wishListModel objektet.

--- src/main/java/com/example/onskeskyen/Repository/userRepository.java
+++ src/main/java/com/example/onskeskyen/Repository/userRepository.java
@@ -3,22 +3,28 @@ package com.example.onskeskyen.Repository;
 import com.example.onskeskyen.Model.userModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
+import com.example.onskeskyen.Model.userModel;
 @Repository
 public class userRepository {
-        @Value("AC25")
-        private String databaseURL;
-        @Value("jdbc:mysql://onskeskyen.mysql.database.azure.com:3306/onskeskyen")
-        private String username;
-        @Value("Konto2500!")
-        private String password;
+    @Value("AC25")
+    private String databaseURL;
+    @Value("jdbc:mysql://onskeskyen.mysql.database.azure.com:3306/onskeskyen")
+    private String username;
+    @Value("Konto2500!")
+    private String password;
 
-       public void createUser(){
-               String createSQL = "insert into (Username, PasswordHash) values (?,?)";
+    public void createUser() {
+        String createSQL = "insert into (Username, PasswordHash) values (?,?)";
+        try (Connection connection = DriverManager.getConnection(databaseURL, username, password)) {
+          PreparedStatement preparedStatement = connection.prepareStatement(createSQL);
+          preparedStatement.setString(1,userModel.getUsername());
+        }
 
-       }
-
+    }
 }
