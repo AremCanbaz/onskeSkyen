d6e912287b997924bcca3a29e58e5656630fb4ab
AC25-C <arem_canbaz@icloud.com>
2024-10-28 18:59:27 +0100

    jeg har lavet wishItem objektet inde i wishListModel objektet.

--- src/main/java/com/example/onskeskyen/Repository/userRepository.java
+++ src/main/java/com/example/onskeskyen/Repository/userRepository.java
@@ -1,15 +1,19 @@
 package com.example.onskeskyen.Repository;
 
 import com.example.onskeskyen.Model.userModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class userRepository {
-    private List<userModel> users = new ArrayList<userModel>();
-
-    public List<userModel> getUsers() {
-        return users;
-    }
+        @Value("AC25")
+        private String databaseURL;
+        @Value(jdbc:mysql://onskeskyen.mysql.database.azure.com:3306/onskeskyen")
+        private String username;
+        @Value("Konto2500!")
+        private String password;
 
 }
