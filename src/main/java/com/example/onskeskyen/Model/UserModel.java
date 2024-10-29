56d2e6947ce3622adfd28b82d72553a476c24888
AC25-C <arem_canbaz@icloud.com>
2024-10-30 00:01:06 +0100

    Databaseen er sat op, user databasen skal laves om igen. css gider ikke at vise igen, må sprøge en lærer om hjælp igen...

--- src/main/java/com/example/onskeskyen/Model/UserModel.java
+++ src/main/java/com/example/onskeskyen/Model/UserModel.java
@@ -1,6 +1,5 @@
 package com.example.onskeskyen.Model;
 
-import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
--- src/main/java/com/example/onskeskyen/Repository/UserRepository.java
+++ src/main/java/com/example/onskeskyen/Repository/UserRepository.java
@@ -3,13 +3,12 @@ package com.example.onskeskyen.Repository;
 import com.example.onskeskyen.Model.UserModel;  // Skift til stor forbogstav
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
