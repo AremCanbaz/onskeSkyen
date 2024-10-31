1315256b016a70eaf5230c974ef93e2f3024fe8d
AC25-C <arem_canbaz@icloud.com>
2024-10-31 01:54:32 +0100

    Forside er lavet til at vise lister frem, men selve login til siden og fremvissing af den unikke bruger er ikke lavet endnu.

--- src/main/java/com/example/onskeskyen/Controller/wishListController.java
+++ src/main/java/com/example/onskeskyen/Controller/wishListController.java
@@ -1,17 +1,28 @@
 package com.example.onskeskyen.Controller;
 
 import com.example.onskeskyen.Model.UserModel;
+import com.example.onskeskyen.Model.wishListModel;
+import com.example.onskeskyen.Service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class wishListController {
+    private WishListService wishListService;
+    @Autowired
+    public void setWishListService(WishListService wishListService) {
+        this.wishListService = wishListService;
+    }
 
     @GetMapping("/forside")
     public String showLoginForm(Model model) {
-        model.addAttribute("wishlist", "")
+        List<wishListModel> wishList = wishListService.getAllWishLists();
+        model.addAttribute("wishlist", wishList);
 
         return "forside";
     }
