82c91c4edbb087c33883419606f10070b4be8213
AC25-C <arem_canbaz@icloud.com>
2024-11-02 00:44:20 +0100

    Problemet er løst, problemet lå i WishItemRepository.createWishlistItem at de forkerte tabel navne fra DB var skrevet forkert, ellers virkelig flot arbejde MO!!!

--- src/main/java/com/example/onskeskyen/Repository/WishItemRepository.java
+++ src/main/java/com/example/onskeskyen/Repository/WishItemRepository.java
@@ -41,7 +41,7 @@ public class WishItemRepository {
 
     }
     public void createWishlistItem(String itemName, String description, Double price, Long wishlistId, String link) {
-        String sqlCreateItem = "INSERT INTO wishlistitems (item_name, description, price, wishlist_id, link) VALUES (?, ?, ?, ?, ?)";
+        String sqlCreateItem = "INSERT INTO wishlistitems (wishitem_name,wishlistitem_desc,wishitem_price, wishlist_ID, wish_item_link) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection connection = DriverManager.getConnection(databaseURLM, userName, password)) {
             PreparedStatement preparedStatement = connection.prepareStatement(sqlCreateItem);
@@ -57,31 +57,5 @@ public class WishItemRepository {
         }
     }
 
-    // Hent alle items til en ønskeliste
-    public List<WishItemModel> findItemsByWishlistId(long wishlistId) {
-        String sql = "SELECT * FROM wishlistitems WHERE wishlist_id = ?";
-        List<WishItemModel> items = new ArrayList<>();
-
-        try (Connection connection = DriverManager.getConnection(databaseURLM, userName, password)) {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setLong(1, wishlistId);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                WishItemModel item = new WishItemModel();
-                item.setItemId(resultSet.getLong("item_id"));
-                item.setItemName(resultSet.getString("item_name"));
-                item.setDescription(resultSet.getString("description"));
-                item.setPrice((int) resultSet.getDouble("price"));
-                item.setWishlistId(resultSet.getLong("wishlist_id"));
-                item.setLink(resultSet.getString("link"));
-
-                items.add(item);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return items;
-    }
 
 }
