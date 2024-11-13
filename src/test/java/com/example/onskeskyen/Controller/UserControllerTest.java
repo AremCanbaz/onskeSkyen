209b633510255842869aa54ffc000c75dcf06297
AC25-C <arem_canbaz@icloud.com>
2024-11-13 22:45:37 +0100

    deployment

--- src/test/java/com/example/onskeskyen/Controller/UserControllerTest.java
+++ src/test/java/com/example/onskeskyen/Controller/UserControllerTest.java
@@ -6,17 +6,16 @@ import com.example.onskeskyen.Service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.mockito.ArgumentMatchers.refEq;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 @WebMvcTest(UserController.class)
 class UserControllerTest {
     @Autowired
